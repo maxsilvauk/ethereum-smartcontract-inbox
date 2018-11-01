f9d49a77da321bf0bc76c06daf1a438920f04d79
Maximillian Silva <maxsilvauk@gmail.com>
2018-11-01 02:08:11 +0000

    Added 0x to start of bytecode

--- deploy.js
+++ deploy.js
@@ -1,3 +1,5 @@
+//
+// Import our bytecode &  our interface
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
@@ -6,7 +8,7 @@ const { interface, bytecode } = require('./compile');
 // we want to connect to.
 const provider = new HDWalletProvider(
   'add enforce fun spring curious tonight divorce minor before celery card trial',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+  'https://rinkeby.infura.io/v3/835fef3984d746eb9ccd66dc5ef48254'
 );
 
 const web3 = new Web3(provider);
@@ -15,11 +17,12 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0])
+  console.log('Attempting to deploy from account', accounts[0]);
 
+  // We need to add '0x' to the start of the bytecode.
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!']})
-    .send({ gas: '1,000,000', from: accounts[0] });
+    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!']})
+    .send({ gas: '1000000', from: accounts[0] });
 
     console.log('Contract deployed to', result.options.address);
 }
